2592402f15d59f3172cefa7c76ac30f9bc6b8808
James Benjamin <james.abenj@gmail.com>
2019-09-20 02:34:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(input)
+  divide_by = (2..(input - 1)).to_a 
+  if input <= 1 
+    false
+  else
+evaluators = divide_by.map do |num|
+  input.to_f / num
+  end
+   if evaluators.include?(Integer)
+    false
+  else
+    true
+     end
+  end
+end 
+  
